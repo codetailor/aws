b86aaf12014cbdac1b7c7718d65277e1547f934e
Ricardo Nunes <ricardonunesdev@gmail.com>
2022-05-04 10:55:56 +0100

    Updated test name

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,7 +4,7 @@ import * as AWS from '../src';
 
 import { BUCKET, KEY } from '../lib/constants';
 
-test('Test', (t) => {
+test('Test S3 download', (t) => {
 	AWS.S3.downloadFile(BUCKET, KEY, 'test-index.html')
 		.then(() => {
 			t.assert(fs.existsSync('test-index.html'), 'File downloaded from S3');
