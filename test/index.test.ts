5c51c632ad19adfcfa7a35082e0dd05b4d7f2c51
Ricardo Nunes <ricardonunesdev@gmail.com>
2022-05-04 10:55:10 +0100

    Updated S3 test

--- test/index.test.ts
+++ test/index.test.ts
@@ -6,7 +6,7 @@ import { BUCKET, KEY } from '../lib/constants';
 
 test('Test', (t) => {
 	AWS.S3.downloadFile(BUCKET, KEY, 'test-index.html')
-		.then(res => {
+		.then(() => {
 			t.assert(fs.existsSync('test-index.html'), 'File downloaded from S3');
 			t.teardown(() => { fs.unlinkSync('test-index.html'); });
 			t.end();
