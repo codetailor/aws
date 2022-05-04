37107260b0a8602b3df8f1d1e47d416592262960
Ricardo Nunes <ricardonunesdev@gmail.com>
2022-05-04 12:12:07 +0100

    Added test for file upload + Updated local test filenames

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,13 +2,22 @@ import test from 'tape';
 import fs from 'fs';
 import * as AWS from '../src';
 
-import { BUCKET, KEY } from '../lib/constants';
+import { BUCKET } from '../lib/constants';
 
 test('Test S3 download', (t) => {
-	AWS.S3.downloadFile(BUCKET, KEY, 'test-index.html')
+	AWS.S3.downloadFile(BUCKET, 'index.html', 'test-download.html')
 		.then(() => {
-			t.assert(fs.existsSync('test-index.html'), 'File downloaded from S3');
-			t.teardown(() => { fs.unlinkSync('test-index.html'); });
+			t.assert(fs.existsSync('test-download.html'), 'File downloaded from S3');
+			t.teardown(() => { fs.unlinkSync('test-download.html'); });
+			t.end();
+		});
+});
+
+test('Test S3 upload', (t) => {
+	fs.writeFileSync('test-upload.html', '', { encoding: 'utf-8' });
+	AWS.S3.uploadFile(BUCKET, 'test-upload.html', [], 'test-upload.html')
+		.then(() => {
+			t.teardown(() => { fs.unlinkSync('test-upload.html'); });
 			t.end();
 		});
 });
